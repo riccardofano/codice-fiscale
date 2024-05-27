caa2c7dbc3b4fc53eee512f87d086a9142cc0e35
Riccardo Fano <rcr.fano@gmail.com>
2024-05-27 13:05:58 +0200

    Initial data structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+enum Gender {
+    Male,
+    Female,
+}
+
+struct Subject {
+    first_name: String,
+    last_name: String,
+    birth_date: String,
+    gender: Gender,
+    birth_place: String,
+    birth_province: String,
+}
+
+struct CodiceFiscale(String);
+
+impl CodiceFiscale {
+    fn parse() -> Result<Self, String> {
+        todo!()
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
