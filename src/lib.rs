fe5fb7fc50c98ccaeb89bf57ab061f749e13c1f7
Riccardo Fano <rcr.fano@gmail.com>
2024-05-27 21:56:02 +0200

    Encode birth date

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use chrono::{Datelike, NaiveDate, Utc};
+
+const MONTH_CODES: [char; 12] = ['A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'P', 'R', 'S', 'T'];
 const VOWELS: [char; 6] = ['A', 'E', 'I', 'O', 'U', ' '];
 const CONSONANTS: [char; 22] = [
     'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X',
@@ -15,7 +18,7 @@ enum Gender {
 struct Subject {
     first_name: String,
     last_name: String,
-    birth_date: String,
+    birth_date: NaiveDate,
     gender: Gender,
     birth_place: String,
     birth_province: String,
@@ -40,6 +43,18 @@ impl Subject {
             format!("{consonants}{vowels}XXX")[..3].to_owned()
         }
     }
+
+    fn birth_date_code(&self) -> String {
+        let mut year = self.birth_date.year().to_string();
+        let month = MONTH_CODES[self.birth_date.month0() as usize];
+        let day = self.birth_date.day();
+
+        format!(
+            "{year_1}{year_0}{month}{day:02}",
+            year_0 = year.pop().unwrap(),
+            year_1 = year.pop().unwrap()
+        )
+    }
 }
 
 struct CodiceFiscale(String);
@@ -55,7 +70,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn last_name_code_enough_consonants() {
+    fn test_last_name_code_enough_consonants() {
         let sub = Subject {
             last_name: "Rossi".into(),
             ..Default::default()
@@ -65,7 +80,7 @@ mod tests {
     }
 
     #[test]
-    fn last_name_code_vowels_needed() {
+    fn test_last_name_code_vowels_needed() {
         let sub = Subject {
             last_name: "Bigi".into(),
             ..Default::default()
@@ -75,7 +90,7 @@ mod tests {
     }
 
     #[test]
-    fn last_name_code_space_inside() {
+    fn test_last_name_code_space_inside() {
         let sub = Subject {
             last_name: "De Rossi".into(),
             ..Default::default()
@@ -85,7 +100,7 @@ mod tests {
     }
 
     #[test]
-    fn last_name_code_short() {
+    fn test_last_name_code_short() {
         let sub = Subject {
             last_name: "Yu".into(),
             ..Default::default()
@@ -95,7 +110,7 @@ mod tests {
     }
 
     #[test]
-    fn first_name_consonants() {
+    fn test_first_name_consonants() {
         let sub = Subject {
             first_name: "Massimo".into(),
             ..Default::default()
@@ -105,7 +120,7 @@ mod tests {
     }
 
     #[test]
-    fn first_name_vowels_needed() {
+    fn test_first_name_vowels_needed() {
         let sub = Subject {
             first_name: "Mario".into(),
             ..Default::default()
@@ -115,7 +130,7 @@ mod tests {
     }
 
     #[test]
-    fn first_name_space_inside() {
+    fn test_first_name_space_inside() {
         let sub = Subject {
             first_name: "Maria Teresa".into(),
             ..Default::default()
@@ -125,12 +140,32 @@ mod tests {
     }
 
     #[test]
-    fn first_name_short() {
+    fn test_first_name_short() {
         let sub = Subject {
             first_name: "Li".into(),
             ..Default::default()
         };
 
         assert_eq!(&sub.first_name_code(), "LIX");
+    }
+
+    #[test]
+    fn test_birth_date_code() {
+        let sub = Subject {
+            birth_date: NaiveDate::from_ymd_opt(2024, 12, 31).unwrap(),
+            ..Default::default()
+        };
+
+        assert_eq!(&sub.birth_date_code(), "24T31");
+    }
+
+    #[test]
+    fn test_birth_date_small_day() {
+        let sub = Subject {
+            birth_date: NaiveDate::from_ymd_opt(2024, 12, 5).unwrap(),
+            ..Default::default()
+        };
+
+        assert_eq!(&sub.birth_date_code(), "24T05");
     }
 }
